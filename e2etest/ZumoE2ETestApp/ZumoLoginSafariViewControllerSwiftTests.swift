9aa3970e300dcebb61df65726f2c17b5009b172e
Jonathan Ngabo <jonathan@sidekickinteractive.com>
2021-04-15 18:11:11 -0400

    changed SKAppKit dependency reference to user version number instead of branch name

--- e2etest/ZumoE2ETestApp/ZumoLoginSafariViewControllerSwiftTests.swift
+++ e2etest/ZumoE2ETestApp/ZumoLoginSafariViewControllerSwiftTests.swift
@@ -6,11 +6,10 @@ class ZumoLoginSafariViewControllerSwiftTests: NSObject {
 
     class func createSafariLoginTest(forProvider provider: String, animated: Bool) -> ZumoTest {
 
-        let result = ZumoTest.createTest(withName: "SafariVC Login for \(provider)", andExecution:
-            {(test, viewController, completion) -> Void in
-                
+        let result = ZumoTest.createTest(withName: "SafariVC Login for \(provider)", andExecution: {(test, viewController, completion) -> Void in
+
                 let client: MSClient? = ZumoTestGlobals.sharedInstance().client
-                
+
                 let loginBlock: MSClientLoginBlock = {(user, error) -> Void in
                     if (error != nil) {
                         test?.addLog("Error logging in: \(error)")
@@ -23,26 +22,26 @@ class ZumoLoginSafariViewControllerSwiftTests: NSObject {
                         completion!(true)
                     }
                 }
-                
+
                 client?.login(withProvider: provider, urlScheme: "ZumoE2ETestApp", controller: viewController!, animated: animated, completion: loginBlock)
             })
-        
+
         return result!
     }
-    
+
     class func createTests() -> [Any] {
-        
+
         var result = [Any]()
-        
+
         result.append(self.createSafariLoginTest(forProvider: "google", animated: true))
         result.append(self.createSafariLoginTest(forProvider: "aad", animated: false))
         result.append(self.createSafariLoginTest(forProvider: "microsoftaccount", animated: false))
         result.append(self.createSafariLoginTest(forProvider: "facebook", animated: false))
         result.append(self.createSafariLoginTest(forProvider: "twitter", animated: false))
-        
+
         return result
     }
-   
+
     class func groupDescription() -> String {
         return "Tests to validate Login via SafariViewController in the client SDK."
     }
